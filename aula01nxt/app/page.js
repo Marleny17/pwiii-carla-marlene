171f7ccc12805a7d06414c6b8d9a1ed073348963
Carla Marlene Alaro Machaca <carlaluffy7@gmail.com>
2024-08-29 22:28:42 -0300

    Aula o1 node JS

---
+++ aula01nxt/app/page.js
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+     <div>
+       <h1>
+         Olá Mundo!
+       </h1>
+       <br/>
+       <p>
+         oila
+       </p>
+     </div>
+    </main>
+  );
+}
